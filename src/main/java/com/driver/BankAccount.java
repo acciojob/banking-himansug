508f7d4b4bffc2a6a2a881123722bb7edfc16b1f
himansug <hbehera09016@gmail.com>
2023-06-17 18:06:19 +0530

    try

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -19,34 +19,29 @@ this.minBalance=minBalance;
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        int sumOfDigit = 0;
-
-
-        while (digits > 0)
-        {
-
-            // Stores the last digit if N
-            int rem;
-            rem = digits % 10;
-
-            // Increment the value of
-            // sumOfDigits
-            sumOfDigit += rem;
-
-            // Update the prodOfDigit
-
-
-            // Divide N by 10
-            digits /= 10;
-        }
-        String s=Integer.toString(digits);
-        if(sumOfDigit==sum){
-            return s;
-        }
-        else{
+        int rem = sum;
+        String accNo = "";
+        if(digits*9 < sum){
             throw new Exception("Account Number can not be generated");
         }
-
+        else{
+            while(digits > 0 && rem > 0){
+                if(rem >= 9){
+                    rem = rem - 9;
+                    accNo = accNo + "9";
+                }
+                else{
+                    accNo = accNo + Integer.toString(rem);
+                    rem = 0;
+                }
+                digits--;
+            }
+            while(digits > 0){
+                accNo = accNo + "0";
+                digits--;
+            }
+            return accNo;
+        }
     }
 
     public void deposit(double amount) {
@@ -56,11 +51,11 @@ balance=balance+amount;
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-if(balance-amount<minBalance){
-    throw new Exception("Insufficient Balance");
-}
-else{balance=balance-amount;
-}
+        if(this.balance - amount >= getMinBalance()){
+            this.balance = this.balance - amount;
+        }
+        else throw new Exception("Insufficient Balance");
+
     }
 
     public String getName() {
